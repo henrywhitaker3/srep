5aaaecfba0991a156d2b41c1dace38fbeded276a
Henry Whitaker <henrywhitaker3@outlook.com>
2023-08-20 18:34:09 +0100

    added tags to scenario metadata

--- internal/metadata/metadata.go
+++ internal/metadata/metadata.go
@@ -12,10 +12,11 @@ var mdJson string
 type Metadata []Scenario
 
 type Scenario struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Difficulty  string `json:"difficulty"`
-	Version     string `json:"version"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Difficulty  string   `json:"difficulty"`
+	Version     string   `json:"version"`
+	Tags        []string `json:"tags"`
 }
 
 func Get() (*Metadata, error) {
--- internal/views/list/list.go
+++ internal/views/list/list.go
@@ -1,6 +1,8 @@
 package list
 
 import (
+	"strings"
+
 	"github.com/fatih/color"
 	"github.com/henrywhitaker3/srep/internal/metadata"
 	"github.com/rodaine/table"
@@ -10,11 +12,11 @@ func NewTable(md *metadata.Metadata) table.Table {
 	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
 	columnFmt := color.New(color.FgYellow).SprintfFunc()
 
-	tbl := table.New("Name", "Difficulty", "Version", "Description")
+	tbl := table.New("Name", "Difficulty", "Version", "Tags")
 	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
 
 	for _, s := range *md {
-		tbl.AddRow(s.Name, s.Difficulty, s.Version, s.Description)
+		tbl.AddRow(s.Name, s.Difficulty, s.Version, strings.Join(s.Tags, ", "))
 	}
 
 	return tbl
