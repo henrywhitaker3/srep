f742f36b16c3c742866d9f3445e51d19f1a28139
Henry Whitaker <henrywhitaker3@outlook.com>
2023-08-20 09:34:02 +0100

    added task for metadata

--- hack/generate_metadata.py
+++ hack/generate_metadata.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+import json
+import glob
+
+combined = []
+
+for file in glob.glob("containers/**/metadata.json"):
+    with open(file) as opened:
+        data = json.load(opened)
+        combined.append(data)
+
+output = json.dumps(combined, indent=4)
+print(output)
