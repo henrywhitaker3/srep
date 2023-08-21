2d1be216f06710e5c261f029d91d5ba888900870
Henry Whitaker <henrywhitaker3@outlook.com>
2023-08-21 11:25:45 +0100

    (containers) skip spacing in banana restart script

--- containers/banana/root/opt/restart.sh
+++ containers/banana/root/opt/restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Kill the currently running redis process
-kill -s 9 $(ps -aux | grep redis-server | head -n 1| awk '{print $2}')
+kill -s 9 $(ps -aux | grep redis-server | head -n 1 | awk '{print $2}')
 
 # Start a new one
 /opt/run/run.sh &
