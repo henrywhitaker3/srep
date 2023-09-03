103dbe7333c4b888cc87a03abae8691214be5a3e
Henry Whitaker <henrywhitaker3@outlook.com>
2023-09-03 13:13:57 +0100

    updated base image with a message

---
+++ containers/base/root/opt/message.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo CiAgICBfX19fX19fX19fXyBfX19fX19fX19fXwogICAgLyAgX19ffCBfX18gXCAgX19ffCBfX18gXAogICAgXCBgLS0ufCB8Xy8gLyB8X18gfCB8Xy8gLwogICAgYC0tLiBcICAgIC98ICBfX3x8ICBfXy8KICAgIC9cX18vIC8gfFwgXHwgfF9fX3wgfAogICAgXF9fX18vXF98IFxfXF9fX18vXF98CgoK | base64 -d
+
+if [ -f "/opt/metadata.json" ]; then
+    description=$(cat /opt/metadata.json | jq -r '.description')
+    printf "$description\n"
+fi
--- hack/generate_metadata.py
+++ hack/generate_metadata.py
@@ -6,6 +6,9 @@ import glob
 combined = []
 
 for file in glob.glob("containers/**/metadata.json"):
+    if file == "containers/base/metadata.json":
+        continue
+
     with open(file) as opened:
         data = json.load(opened)
         combined.append(data)
